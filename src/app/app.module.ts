42032474bc2ed70c3789912d877d394107961db9
Amit Buhnik <P0020755@ness-tech.co.il>
2018-07-26 14:16:55 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header.component';
+import { ShoppingListComponent } from './shopping/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-list-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
