43f7a0c12331cbe13c962c3e4bd635d82e8e0f87
Amit Buhnik <P0020755@ness-tech.co.il>
2018-07-26 17:49:57 +0300

    bootstrap

--- src/app/shopping/shopping-list.component.ts
+++ src/app/shopping/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('bread', 5),
+    new Ingredient('Green Tea', 2)
+  ];
 
   constructor() { }
 
