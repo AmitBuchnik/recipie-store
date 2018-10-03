78a6fa90145fcebcd44c188735af6368bb93cb64
Amit Buhnik <P0020755@ness-tech.co.il>
2018-10-03 11:54:38 +0300

    angular universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store'; 
@@ -26,14 +27,15 @@ import { AuthEffects } from './auth/ngrx-store/auth.effects';
     // the page.
     BrowserModule.withServerTransition({ appId: 'my-app' }),
     HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
     CoreModule,
     SharedModule,
     AuthModule,
     ShoppingListModule,
-    StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([AuthEffects]),
-    StoreRouterConnectingModule,
-    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
