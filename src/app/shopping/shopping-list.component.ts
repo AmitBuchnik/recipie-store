733a691d5675622850e3734137d290526a41b0de
Amit Buhnik <P0020755@ness-tech.co.il>
2018-10-17 12:54:55 +0300

    animation

--- src/app/shopping/shopping-list.component.ts
+++ src/app/shopping/shopping-list.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {  Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes,
+  group
+} from '@angular/animations';
 
 import * as fromShoppingList from '../shopping/ngrx-store/shopping-list.reducers';
 import * as ShoppingListActions from './ngrx-store/shopping-list.actions';
@@ -9,7 +18,71 @@ import * as fromApp from '../ngrx-store/app.reducers';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  animations: [
+    trigger('list1', [
+      state('inDom', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      // void is reserve word for element not in the DOM
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+    trigger('list', [
+      state('inDom', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      // void is reserve word for element not in the DOM
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])
+      ])
+    ])
+  ]
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   shoppingListState: Observable<fromShoppingList.IState>;
